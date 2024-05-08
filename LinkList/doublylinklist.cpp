2314ea56688d1923a6ca1a94e759440505840471
Dheeraj Rauniyar <dheerajrauniyar123@gmail.com>
2024-05-08 11:17:48 +0530

    Implemented doubly linked list operations: creation, insertion, and deletion at start, end, mid, and any position.

--- LinkList/doublylinklist.cpp
+++ LinkList/doublylinklist.cpp
@@ -13,23 +13,110 @@ public:
         this->prev = NULL;
         this->next = NULL;
     }
+    ~Node()
+    {
+        int value = this->data;
+        if (next != NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+        cout << "memory is free for node with data: " << value << endl;
+    }
 };
-
-void insertAtHead(Node *&head, int d)
+// inseart at head
+void insertAtHead(Node *&head, Node *&tail, int d)
 {
-    Node *temp = new Node(d);
-    temp->next = head;
-    head->prev = temp;
-    head = temp;
+    if (head == NULL)
+    {
+        Node *temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
 }
-void insertAtTail(Node *tail, int d)
+// inseart at tail
+void insertAtTail(Node *&tail, Node *&head, int d)
 {
-    Node *temp = new Node(d);
-    tail->next = temp;
-    temp->prev = tail;
-    temp = tail;
+    if (tail == NULL)
+    {
+
+        Node *temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
 }
 
+// insert at a position
+void insertAtPosition(Node *&head, Node *&tail, int position, int d)
+{ // insert at start
+    if (position == 1)
+    {
+        insertAtHead(head, tail, d);
+        return;
+    }
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // insert at last
+
+    if (temp->next == NULL)
+    {
+
+        insertAtTail(tail, head, d);
+        return;
+    }
+    // insert at mid
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next->prev = nodeToInsert;
+    temp->next = nodeToInsert;
+    nodeToInsert->prev = temp;
+}
+
+void deleteNode(Node *&head, int position)
+{
+    if (position == 1)
+    {
+        Node *temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }else{
+        Node* curr= head;
+        Node* previ = NULL;
+        int cnt  =1;
+        while(cnt<position-1){
+            previ =curr;
+            curr = curr->next;
+            cnt++;
+        }
+        curr->prev = NULL;
+        previ->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+// print the node
 void print(Node *head)
 {
     Node *temp = head;
@@ -40,7 +127,7 @@ void print(Node *head)
     }
     cout << endl;
 }
-
+// length of the node
 int getLength(Node *head)
 {
     Node *temp = head;
@@ -55,20 +142,29 @@ int getLength(Node *head)
 
 int main()
 {
-    Node *node1 = new Node(10);
-    Node *head = node1;
-    Node *tail = node1;
+    // Node *node1 = new Node(10);
+    Node *head = NULL;
+    Node *tail = NULL;
     print(head);
     // cout << getLength(head);
 
-    insertAtHead(head, 12);
+    insertAtHead(head, tail, 12);
     print(head);
-    insertAtHead(head, 13);
+    insertAtHead(head, tail, 13);
     print(head);
 
-    insertAtTail(tail, 15);
+    insertAtTail(tail, head, 15);
     print(head);
-    insertAtTail(tail, 16);
+    insertAtTail(tail, head, 16);
+    print(head);
+    insertAtPosition(head, tail, 3, 102);
+    print(head);
+    insertAtPosition(head, tail, 1, 103);
+    print(head);
+    insertAtPosition(head, tail, 7, 104);
+    print(head);
+
+    deleteNode(head,8);
     print(head);
     return 0;
 }
